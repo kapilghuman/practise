f5c78e1996eca5a1ea72ace85590ffa2f2e9f6d0
Kapil Ghuman <kapil.ghuman007@gmail.com>
2018-06-08 02:19:09 +0530

    Create DecimalToRoman.cpp

---
+++ DecimalToRoman.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int value(char c)
+{
+    char alpha[]={'I','V','X','L','C','D','M'};
+    int arr[]={1,5,10,50,100,500,1000};
+    int val;
+    for(int i=0;i<7;i++)
+    {
+        if(alpha[i]==c)
+        {
+            val=arr[i];
+            break;
+        }
+    }
+    return val;
+}
+int main()
+{
+    
+    string t;
+    cin>>t;
+    long p,sum=0;
+    int arr[t.size()];
+    for(int i=0;i<t.size();i++)
+    {
+        arr[i]=value(t[i]);
+    }
+    for(int i=0;i<t.size()-1;i++)
+    {
+        if(arr[i]>=arr[i+1])
+        {
+            p=arr[i];
+        }
+        else
+        {
+            p=-arr[i];
+        }
+        sum=sum+p;
+    }
+    sum=sum+arr[t.size()-1];
+    cout<<sum;
+}
