b1ade7209c8d1dd0e5eb32c6f6c513f5c2fbf7d9
Kapil Ghuman <kapil.ghuman007@gmail.com>
2018-06-11 00:10:25 +0530

    Update sequenceSum.cpp

--- sequenceSum.cpp
+++ sequenceSum.cpp
@@ -1,1 +1,28 @@
-a
+#include<iostream>
+using namespace std;
+int sequenceSum(int start, int end, int step)
+{
+  if(start>end)
+  {
+      return 0;
+  }
+  else
+  {
+      long sum=0;
+      long k=start;
+        while(k<=end)
+        {
+            sum=sum+k;
+            k=k+step;
+            
+           //cout<<"value of k"<<k<<"value of sum"<<sum<<endl;
+        }
+        return sum;
+  }
+}
+int main()
+{
+    int start,end,step;
+    cin>>start>>end>>step;
+    cout<<sequenceSum(start, end, step);
+}
